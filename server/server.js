0662ef85ec8e304f149768079630c8aa4e320251
Rahulsaini8989iu <rahulsaini8989iu@gmail.com>
2025-08-31 16:26:09 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import e from 'express';
+import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
+
 
 const app = express();
 const port = 3000;
 
-//middleware
+await connectDB()
 
+//middleware
 app.use(express.json())
 app.use(cors())
+app.use(clerkMiddleware())
+
+
+//API  ROutes
+app.get('/', (req , res) => res.send('server is live!'))
+app.use('/api/inngest', serve({ client: inngest, functions }))
+
+
+
+app.listen(port, ()=> console.log(`Server is running on http://localhost:${port}`));
